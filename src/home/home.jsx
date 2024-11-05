f7716e0f118279856e59ceee40bb2e59e48be7b5
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-11-05 10:50:03 +0100

    setup

---
+++ src/home/home.jsx
@@ -0,0 +1,9 @@
+
+export const Homepage = () => {
+    return(
+        <div className="container mx-auto">
+            <p className="text-lg text-blue-600 text-center">Billie elilsh</p>
+
+        </div>
+    )
+}
