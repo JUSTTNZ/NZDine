20916c3e63c145cea4d256f2bcb7f5b7beebfd83
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-11-08 12:20:23 +0100

    added menu.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Homepage } from './pages/home/home'
 import  About from './pages/about/about'
-
-import Menu from './pages/menu/menu'
 import Pages from './pages/pages/pages'
 import Service from './pages/service/service'
 import { Footer } from './footer'
 import { Contact } from './contact/contact'
+import { Menu } from './menu/menu'
 
 
 function App() {
