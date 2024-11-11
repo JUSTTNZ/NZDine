b3ff6f6df34d998a7cf03bc5b371f028977db1a0
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-11-11 04:21:13 -0800

    done with the home web page, remain the responsiveness

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -1,19 +1,56 @@
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { FaQuoteLeft } from "react-icons/fa"
 import client1 from '../img/testimonial-1.jpg';
 import client2 from '../img/testimonial-2.jpg';
 import client3 from '../img/testimonial-3.jpg';
 import client4 from '../img/testimonial-4.jpg';
-import { useEffect, useState } from "react";
 const Clients = [
-    { Id: 1, ClientsName: 'Princess', Img: client1, Profession: 'DevOps', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, <br /> sed stet lorem sit clita duo justo magna dolore erat amet' },
-    { Id: 2, ClientsName: 'Great', Img: client2, Profession: 'Frontend Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, <br /> sed stet lorem sit clita duo justo magna dolore erat amet' },
-    { Id: 3, ClientsName: 'Vincent', Img: client3, Profession: 'Backend Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, <br /> sed stet lorem sit clita duo justo magna dolore erat amet' },
-    { Id: 4, ClientsName: 'Possible', Img: client4, Profession: 'Frontend Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, <br /> sed stet lorem sit clita duo justo magna dolore erat amet' },
-    { Id: 5, ClientsName: 'Collins', Img: client2, Profession: 'Software Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, <br /> sed stet lorem sit clita duo justo magna dolore erat amet' },
-    { Id: 6, ClientsName: 'Promise', Img: client4, Profession: 'Full Stack Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, <br /> sed stet lorem sit clita duo justo magna dolore erat amet' },
+    { Id: 1, ClientsName: 'Princess', Img: client1, Profession: 'DevOps', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,sed stet lorem sit clita duo justo magna dolore erat amet' },
+    { Id: 2, ClientsName: 'Great', Img: client2, Profession: 'Frontend Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet' },
+    { Id: 3, ClientsName: 'Vincent', Img: client3, Profession: 'Backend Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,sed stet lorem sit clita duo justo magna dolore erat amet' },
+    { Id: 4, ClientsName: 'Possible', Img: client4, Profession: 'Frontend Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,sed stet lorem sit clita duo justo magna dolore erat amet' },
+    { Id: 5, ClientsName: 'Collins', Img: client2, Profession: 'Software Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,sed stet lorem sit clita duo justo magna dolore erat amet' },
+    { Id: 6, ClientsName: 'Promise', Img: client4, Profession: 'Full Stack Developer', Description: 'Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. <br /> Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet' },
 ]
 const Testimonial = () => {
-    const [currentSlide, setCurrentSlide] = useState(0)
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true,
+        centerPadding: "0", // Ensures no padding on the sides
+        beforeChange: (_, next) => setCurrentSlide(next), 
+        customPaging: (i) => (
+            <div className={`dot ${i === currentSlide ? "active" : ""}`} />
+        ),
+        responsive: [
+        {
+            breakpoint: 1024, // Tablet and below
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: "0",
+            }
+        },
+        {
+            breakpoint: 600, // Mobile devices
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: "0",
+            }
+        }
+    ]
+      };
+    
+    const [currentSlide, setCurrentSlide] =  useState(0)
     const slideCount = Clients.length;
 
     useEffect(() => {
@@ -25,17 +62,15 @@ const Testimonial = () => {
 
     const getSlideClasses = (index) => { 
         if(index === currentSlide) {
-            return 'bg-[#FEA116] text-white border rounded rounded-lg scale-100 z-10 shadow-lg transform translate-x-0';
-        }else if (index === (currentSlide + 1) % slideCount) {
-            return 'bg-white text-black  scale-95 shadow-md opacity-80 transform translate-x-full';
-        }else if (index === (currentSlide -1 + slideCount) % slideCount){
-            return 'bg-white text-black  scale-95 shadow-md opacity-80 transform -translate-x-full';
+            return `active-slide bg-[#FEA116] text-white border rounded-lg  z-10 `;
+        }else if (index === (currentSlide + 1) % slideCount || index === (currentSlide - 1 + slideCount) % slideCount) {
+            return ' non-active-slide bg-white text-black  scale-95 shadow-md ';
         } else {
-            return 'bg-white text-black  scale-95 shadow-md opacity-80 transform translate-x-full'
+            return 'bg-white text-black  scale-95 shadow-md'
         }
     }
   return (
-    <div className="mb-10">
+    <div className="mb-20">
         <div>
             <div className="text-center mt-24 ">
                 <h5 className="font-paci text-[#FEA116]  font-[400] text-left text-h4 mb-4 relative inline-block">
@@ -46,16 +81,26 @@ const Testimonial = () => {
                 <h1 className="mb-5 text-query text-[#0F172B] font-nun font-bold leading-custom">Our Clients Say!!!</h1>
             </div>
         </div>
-        <div className="flex flex-col items-center mt-8 relative">
-            <div className="flex space-x-4 px-24 items-center overflow-hidden w-full mr-4">
+        <div className="w-3/4 m-auto">
+            <div className="pt-20 ">
+                <Slider {...settings}>
                 { Clients.map((client, index) => (
                     <div 
                     key={client.Id}
-                    className={`flex-shrink-0 w-1/3 p-6 border rounded rounded-lg transition-all duration-500 ease-in-out transform ${getSlideClasses(index)}`}
+                    className={`flex-shrink-0 w-1/3 p-6 mb-8 border rounded rounded-lg ${getSlideClasses(index)}`}
                     >
                         <div>
                         <a href="">
-                            <FaQuoteLeft size={24} className="mb-4"/>
+                            <FaQuoteLeft
+                                size={24}
+                                className={`mb-4 ${
+                                    currentSlide === index || 
+                                    index === (currentSlide + 1) % slideCount &&
+                                    index === (currentSlide - 1 + slideCount) % slideCount
+                                        ? 'text-[#fff]'
+                                        : 'text-[#FEA116]'
+                                }`}
+                            />
                         </a>
                         <p className="mb-6">
                             {client.Description}
@@ -73,6 +118,7 @@ const Testimonial = () => {
                         </div>
                     </div>
                 ))}
+                </Slider>
             </div>
         </div>
     </div>
